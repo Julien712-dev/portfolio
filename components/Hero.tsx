93af961e56bc1f75cbff6fb946632fc32519b685
Julien712-dev <miclejulien@gmail.com>
2024-09-11 14:42:53 -0700

    changed some files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -59,7 +59,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Julien, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Julien, a Senior Full-stack Developer Specialized in Next.js.
           </p>
 
           <a href="#about">
